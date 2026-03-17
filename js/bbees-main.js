8e8c63a4efb932aab43bc696441b2162b1939ccb
wightryder <github@solentsoftware.co.uk>
2026-03-17 12:29:00 +0000

    bbees Oakwood v4 updated gitignore

--- js/bbees-main.js
+++ js/bbees-main.js
@@ -1,22 +1,21 @@
 class SiteFooter extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<div class="item-footer">
-        <p><strong>Award Winning Honey</strong></p>
-        <p>&copy; 2024 Bavington Bees</p>
-    </div>`
-        const script = document.createElement('script')
-        script.dataset.goatcounter = 'https://bavingtonbees.goatcounter.com/count'
-        script.src = '//gc.zgo.at/count.js'
-        script.async = true
-        document.head.appendChild(script)
+        this.innerHTML = `
+            <div class="item-footer">
+                <p><strong>Award Winning Honey</strong></p>
+                <p>&copy; 2024 Bavington Bees</p>
+            </div>
+        `;
     }
 }
-customElements.define('site-footer', SiteFooter)
 
-const fabs = document.querySelectorAll('.fab')
+customElements.define('site-footer', SiteFooter);
+
+const fabs = document.querySelectorAll('.fab');
 
 window.addEventListener('scroll', () => {
     fabs.forEach(fab => {
-        fab.classList.toggle('fab--visible', window.scrollY > 300)
-    })
-})
+        fab.classList.toggle('fab--visible', window.scrollY > 300);
+    });
+});
+
